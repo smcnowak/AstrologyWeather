941d1426da344c4fdadf6083ecb1db171e1835b6
Savannah Nowak <smc.nowak25@gmail.com>
2022-09-04 14:27:05 -0400

    found the right packages and tutorial

--- app.js
+++ app.js
@@ -1,105 +1,58 @@
 const express = require("express");
-const https = require("https");
-const fetch = require("node-fetch");
-
+const request = require("request");
 const app = express();
-app.use(express.urlencoded({extended: true}));
+
+require("dotenv").config();
+
+const apiKey = `${process.env.API_KEY}`;
+
 app.use(express.static("Public"));
-app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 
-const apiKey = "08192e4b4109f1ee6dd293f4f4721cf5";
-const unit = "metric";
-const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
-var query = "Temperature";
-
-app.get('/', (req, res) => {
-
-
-  res.render("index", {Temperature: query});
-
+app.get("/", function (req, res) {
+  res.render("index", {weather: null, error: null});
 });
 
 app.post("/", function(req, res){
-query = req.body.cityName;
 
-fetch(url)
-  .then(response =>{
-    return response.json();
-  })
-  .then(data=>{
-    const { temp } = data.main;
-    const { description, icon } = data.weather[0];
+let city = req.body.cityName;
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-    const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+  request(url, function(err, response, body){
 
-    temperature.textContent = temp;
-    conditions.textContent = description;
-    imgIcon.src = imageURL;
+    if (err) {
+        res.render("index", {weather: null, error: 'Error, please try again'});
+    } else {
+        let weather = JSON.parse(body);
+          console.log(weather);
+        if (weather.main == undefined){
+          res.render("index", {weather: null, error: 'Error, please try again'});
+        } else {
+          let place = `${weather.name}, ${weather.sys.country}`,
+            weatherTemp =`${weather.main.temp}`,
+            weatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
+            weatherDescription = `${weather.weather[0].description}`,
+            main = `${weather.weather[0].main}`;
 
-  });
-
- res.redirect("/");
+            res.render("index",{
+              weather: weather,
+              place: place,
+              temp: weatherTemp,
+              icon: weatherIcon,
+              description: weatherDescription,
+              main: main,
+              error: null,
+            });
+          }
+      }
+    });
 });
 
-// const imgIcon = document.getElementById("weatherIcon")
-// const temperature = document.querySelector(".temperatureDegree")
-// const conditions = document.queryselector(".weatherDescription")
-//
-//
-// app.post("/", function(req,res){
-//   const query = req.body.cityName;
-//   const apiKey = "08192e4b4109f1ee6dd293f4f4721cf5";
-//   const unit = "metric";
-//   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+
 //   const sign = req.body.astroName;
 //   const astroURL = "https://ohmanda.com/api/horoscope/" + sign +"/#";
-//
-//
-// fetch(url)
-//   .then(response =>{
-//     return response.json();
-//   })
-//   .then(data=>{
-//     const { temp } = data.main;
-//     const { description, icon } = data.weather[0];
-//
-//     const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-//
-//     temperature.textContent = temp;
-//     conditions.textContent = description;
-//     imgIcon.src = imageURL;
-//
-//   });
-//
-// });
-
-
-// https.get(astroURL, function(response){
-//   response.on("data", function(data){
-//     const astroData = JSON.parse(data)
-//     const yourReading = astroData.horoscope
-//     res.write("<p>" + yourReading + "</p>");
-//     res.send();
-//   })
-// })
-//
-// https.get(url, function(response){
-//
-//   response.on("data", function(data){
-//     const weatherData = JSON.parse(data)
-//     const temp = weatherData.main.temp
-//     const description = weatherData.weather[0].description
-//     const icon = weatherData.weather[0].icon
-//     const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-//     })
-//   })
-
-// res.write("<p>The weather is curently " + description + "</p>");
-// res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celsius.</h1>");
-// res.write("<img src=" + imageURL + ">");
-// res.send();
-
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000.");
