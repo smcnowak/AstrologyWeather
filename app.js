dfc16f8b1d2316fd8910a4eca828986b8ae93ae1
Savannah Nowak <smc.nowak25@gmail.com>
2022-09-04 20:23:44 -0400

    added second api...it almost works too

--- app.js
+++ app.js
@@ -15,18 +15,30 @@ app.get("/", function (req, res) {
   res.render("index", {weather: null, error: null});
 });
 
+
 app.post("/", function(req, res){
 
 let city = req.body.cityName;
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let sign = req.body.astroName;
+let astroURL = `https://ohmanda.com/api/horoscope/${sign}/#`;
+let astroReading = "";
+
+  request(astroURL, function(err, response, body){
+    if (err) {
+        res.render("index", {weather: null, error: 'Error, please try again'});
+      } else {
+        let astro = JSON.parse(body);
+        let astroReading = `${astro.horoscope}`;
+        console.log (astroReading);
+      }
+    });
 
   request(url, function(err, response, body){
-
     if (err) {
         res.render("index", {weather: null, error: 'Error, please try again'});
     } else {
         let weather = JSON.parse(body);
-          console.log(weather);
         if (weather.main == undefined){
           res.render("index", {weather: null, error: 'Error, please try again'});
         } else {
@@ -34,15 +46,24 @@ let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api
             weatherTemp =`${weather.main.temp}`,
             weatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
             weatherDescription = `${weather.weather[0].description}`,
-            main = `${weather.weather[0].main}`;
+            main = `${weather.weather[0].main}`,
+            weatherFahrenheit;
+            weatherFahrenheit = (weatherTemp * 9) / 5 + 32;
+
+            function roundToTwo(num) {
+              return +(Math.round(num + "e+2") + "e-2");
+            }
+            weatherFahrenheit = roundToTwo(weatherFahrenheit);
 
             res.render("index",{
               weather: weather,
               place: place,
               temp: weatherTemp,
+              fahrenheit: weatherFahrenheit,
               icon: weatherIcon,
               description: weatherDescription,
               main: main,
+              reading: astroReading,
               error: null,
             });
           }
