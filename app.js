0c0c5150f260fd226cf3d4e19d2c3d3c3eb18db6
Savannah Nowak <smc.nowak25@gmail.com>
2022-09-09 21:47:51 -0400

    FINALLY both APIs displayed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const request = require("request");
 const app = express();
+const fetch = require("node-fetch");
 
 require("dotenv").config();
 
@@ -15,61 +16,133 @@ app.get("/", function (req, res) {
   res.render("index", {weather: null, error: null});
 });
 
-
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
 
 let city = req.body.cityName;
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 let sign = req.body.astroName;
 let astroURL = `https://ohmanda.com/api/horoscope/${sign}/#`;
-let astroReading = "";
 
-  request(astroURL, function(err, response, body){
-    if (err) {
-        res.render("index", {weather: null, error: 'Error, please try again'});
-      } else {
-        let astro = JSON.parse(body);
-        let astroReading = `${astro.horoscope}`;
-        console.log (astroReading);
-      }
-    });
 
-  request(url, function(err, response, body){
-    if (err) {
-        res.render("index", {weather: null, error: 'Error, please try again'});
-    } else {
-        let weather = JSON.parse(body);
-        if (weather.main == undefined){
-          res.render("index", {weather: null, error: 'Error, please try again'});
-        } else {
-          let place = `${weather.name}, ${weather.sys.country}`,
-            weatherTemp =`${weather.main.temp}`,
-            weatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
-            weatherDescription = `${weather.weather[0].description}`,
-            main = `${weather.weather[0].main}`,
-            weatherFahrenheit;
-            weatherFahrenheit = (weatherTemp * 9) / 5 + 32;
+  fetch(url)
+      .then((response) =>
+      response.json())
+      .then((data)=>{
+        const weather = data;
+        const { temp } = data.main;
+        const place = data.name;
+        const { description, icon } = data.weather[0];
+        const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+        const fahrenheit = (temp *9) /5 +32;
+      app.locals.weather = weather;
+      app.locals.temp = temp;
+      app.locals.place = place;
+      app.locals.description = description;
+      app.locals.iconURL = iconURL;
+      app.locals.fahrenheit = fahrenheit;
+      })
+      .catch((error) =>
+     console.error('Error:', error));
 
-            function roundToTwo(num) {
-              return +(Math.round(num + "e+2") + "e-2");
-            }
-            weatherFahrenheit = roundToTwo(weatherFahrenheit);
+  fetch(astroURL)
+      .then((response) =>
+      response.json())
+      .then((data) => {
+        const reading = data.horoscope;
+        app.locals.reading = reading;
+      })
+      .catch((error) =>
+     console.error('Error:', error));
 
-            res.render("index",{
-              weather: weather,
-              place: place,
-              temp: weatherTemp,
-              fahrenheit: weatherFahrenheit,
-              icon: weatherIcon,
-              description: weatherDescription,
-              main: main,
-              reading: astroReading,
-              error: null,
-            });
-          }
-      }
-    });
+     res.render("index",{
+       weather: app.locals.weather,
+       place: app.locals.place,
+       temp: app.locals.temp,
+       fahrenheit: app.locals.fahrenheit,
+       icon: app.locals.iconURL,
+       description: app.locals.description,
+       reading: app.locals.reading,
+       error: null,
+      });
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // request(astroURL, function(err, response, body){
+  //   if (err) {
+  //       res.render("index", {weather: null, error: 'Error, please try again'});
+  //     } else {
+  //       let astro = JSON.parse(body);
+  //       let astroReading = `${astro.horoscope}`;
+  //       return astroReading;
+  //     }
+  //   });
+  //
+  //
+  // request(url, function(err, response, body){
+  //   if (err) {
+  //       res.render("index", {weather: null, error: 'Error, please try again'});
+  //   } else {
+  //       let weather = JSON.parse(body);
+  //       if (weather.main == undefined){
+  //         res.render("index", {weather: null, error: 'Error, please try again'});
+  //       } else {
+  //         let place = `${weather.name}, ${weather.sys.country}`,
+  //           weatherTemp =`${weather.main.temp}`,
+            // weatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
+  //           weatherDescription = `${weather.weather[0].description}`,
+  //           main = `${weather.weather[0].main}`,
+  //           weatherFahrenheit;
+  //           weatherFahrenheit = (weatherTemp * 9) / 5 + 32;
+  //
+  //           function roundToTwo(num) {
+  //             return +(Math.round(num + "e+2") + "e-2");
+  //           }
+  //           weatherFahrenheit = roundToTwo(weatherFahrenheit);
+  //
+  //           let reading = astroReading;
+
+            // res.render("index",{
+            //   weather: weather,
+            //   place: place,
+            //   temp: weatherTemp,
+            //   fahrenheit: weatherFahrenheit,
+            //   icon: weatherIcon,
+            //   description: weatherDescription,
+            //   main: main,
+            //   reading: reading,
+            //   error: null,
+            // });
+//           }
+//       }
+//     });
+// });
+
+
 
 
 //   const sign = req.body.astroName;
